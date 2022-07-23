ca5daf22860d12c032bc924fd71c65777719fc87
Sithu Aung <sithuaungdev01@gmail.com>
2022-07-24 04:21:53 +0630

    Stage

---
+++ lib/src/features/donation/blood_donation_report.dart
@@ -0,0 +1,95 @@
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:merchant/donation_list_response.dart';
+import 'package:merchant/responsive.dart';
+import 'package:merchant/src/features/donation/donation_chart_by_blood.dart';
+import 'package:merchant/src/features/donation/donation_chart_by_desease.dart';
+import 'package:merchant/src/features/donation/donation_chart_by_hospital.dart';
+import 'package:merchant/utils/Colors.dart';
+
+class BloodDonationReportScreen extends StatefulWidget {
+  final List<DonationData> data;
+  final int month;
+  final String year;
+  const BloodDonationReportScreen(
+      {Key? key, required this.data, required this.month, required this.year})
+      : super(key: key);
+
+  @override
+  State<BloodDonationReportScreen> createState() =>
+      _BloodDonationReportScreenState();
+}
+
+class _BloodDonationReportScreenState extends State<BloodDonationReportScreen> {
+  @override
+  void initState() {
+    super.initState();
+    print(widget.data.length);
+  }
+
+  List<String> months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: NeumorphicTheme.of(context)?.current!.accentColor,
+        appBar: AppBar(
+          flexibleSpace: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [primaryColor, primaryDark],
+          ))),
+          centerTitle: true,
+          title: Padding(
+            padding: const EdgeInsets.only(top: 4),
+            child: Text(
+                widget.year +
+                    " " +
+                    months[widget.month] +
+                    " သွေးလှူဒါန်းမှုမှတ်တမ်း",
+                textScaleFactor: 1.0,
+                style: TextStyle(
+                    fontSize: Responsive.isMobile(context) ? 15 : 17,
+                    color: Colors.white)),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: DonationChartByBlood(
+                    data: widget.data,
+                  )),
+              const SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                  flex: 1, child: DonationChartByHospital(data: widget.data)),
+              const SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                  flex: 1, child: DonationChartByDisease(data: widget.data)),
+            ],
+          ),
+        ));
+  }
+}
